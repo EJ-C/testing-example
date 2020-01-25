d988018175e183ecde370321839a950dc31491e4
EJ-C <10ngweekendofficial@gmail.com>
2020-01-25 11:47:53 -0500

    testing update

--- testing-example/script.test.js
+++ testing-example/script.test.js
@@ -1,3 +1,29 @@
-it('this is a test', () => {
-	
+const googleSearch = require('./script');
+
+dbMock = [
+'dog.com',
+'cheesepuff.com',
+'disnet.com',
+'dogpictures.com'
+];
+
+
+describe('googleSearch', () => {
+	it('this is a test', () => {
+		expect('hellooo').toBe('hellooo')
+		googleSearch('testtest', dbMock);
+	})
+	it('is weird searching google test', () => {
+		expect(googleSearch('testtest', dbMock)).toEqual([]);
+		expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+	})
+
+	it('work with undefined and null input', () => {
+		expect(googleSearch(undefined, dbMock)).toEqual([]);
+		expect(googleSearch(null, dbMock)).toEqual([]);
+	})
+
+	it('does not return more than 3 matches', () => {
+		expect(googleSearch('.com', dbMock).length).toEqual(3);
+	})
 })
