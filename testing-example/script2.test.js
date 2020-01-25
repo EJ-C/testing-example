a50d0d5f4f84eec86e43a3be588cb83b01e05aae
EJ-C <10ngweekendofficial@gmail.com>
2020-01-25 12:55:45 -0500

    Add asynchronous testing

---
+++ testing-example/script2.test.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+// if it's not a package you need to type path
+const swapi = require('./script2');
+
+it('calls swapi to get people', () => {
+	expect.assertions(1)
+	return swapi.getPeople(fetch).then(data => {
+		expect(data.count).toEqual(87);
+		
+	})
+})
+
+it('calls swapi to get people with a promise', () => {
+	expect.assertions(2)
+	return swapi.getPeoplePromise(fetch).then(data => {
+		expect(data.count).toEqual(87)
+		expect(data.results.length).toBeGreaterThan(5)
+	})
+})
